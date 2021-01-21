c35118d23aab00516ccf3fba340cf1a86bcdd879
ZhenjaHorbach <HorbachZhenja@yandex.by>
2021-01-21 23:48:24 +0300

    feat: authorization setup

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	MONGOURI: "mongodb+srv://zhenja_3033:GAZM0ZuZIUkmQI6x@cluster0.avrr0.mongodb.net/<dbname>?retryWrites=true&w=majority"
+	MONGOURI: "mongodb+srv://zhenja_3033:GAZM0ZuZIUkmQI6x@cluster0.avrr0.mongodb.net/<dbname>?retryWrites=true&w=majority",
+	JWT_SECRET: "sadasdasdasdsad"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../keys');
+const requireLogin = require('../middleware/requireLogin');
 
 router.get('/', (req, res) => {
 	res.send('hello');
@@ -17,19 +21,52 @@ router.post('/signup', (req, res) => {
 		if (savedUser) {
 			return res.status(422).json({ error: 'user already exists with that email' });
 		}
-		const user = new User({ email, password, name });
+		bcrypt.hash(password, 12)
+			.then(hashedpassword => {
+				const user = new User({ email, password: hashedpassword, name });
 
-		user.save()
-			.then(user => {
-				res.json({ message: 'saved successfully' });
+				user.save()
+					.then(user => {
+						res.json({ message: 'saved successfully' });
+					})
+					.catch(err => {
+						console.log(err);
+					})
 			})
 			.catch(err => {
 				console.log(err);
 			})
 	})
-		.catch(err => {
-			console.log(err);
+});
+
+router.post('/signin', (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(422).json({ error: 'provide email or password' });
+	}
+	User.findOne({ email: email })
+		.then(savedUser => {
+			if (!savedUser) {
+				return res.status(422).json({ error: 'Invalid Email or password' });
+			}
+			bcrypt.compare(password, savedUser.password)
+				.then(doMatch => {
+					if (doMatch) {
+						res.json({ message: 'successfully signed in' });
+						const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+						res.json({ token });
+					} else {
+						return res.status(422).json({ error: 'Invalid Email or password' });
+					}
+				})
+				.catch(err => {
+					console.log(err);
+				})
 		})
 });
 
+router.post('/protected', requireLogin, (reg, res) => {
+	res.send('Hello user');
+
+});
 module.exports = router;
