0b1d3def2538aa5667b5d70dfaf69c45d7ed330e
ZhenjaHorbach <HorbachZhenja@yandex.by>
2021-01-21 21:12:23 +0300

    feat: registration setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
 
 router.get('/', (req, res) => {
 	res.send('hello');
 });
 
 router.post('/signup', (req, res) => {
-	const {name, email,password} = req.body;
+	const { name, email, password } = req.body;
 	if (!email || !password || !name) {
-		return res.status(422).json({error:'please add all info'});
+		return res.status(422).json({ error: 'please add all info' });
 	}
-	res.json({message:'successfuly !!!'})
+
+	User.findOne({ email: email }).then((savedUser) => {
+		if (savedUser) {
+			return res.status(422).json({ error: 'user already exists with that email' });
+		}
+		const user = new User({ email, password, name });
+
+		user.save()
+			.then(user => {
+				res.json({ message: 'saved successfully' });
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	})
+		.catch(err => {
+			console.log(err);
+		})
 });
 
 module.exports = router;
