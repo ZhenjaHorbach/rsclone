653bc929c242fe9b930b6a3688ec985b48f9858d
ZhenjaHorbach <HorbachZhenja@yandex.by>
2021-01-23 15:49:53 +0300

    feat: add the function of registration and login to the site

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { MONGOURI } = require('./keys');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 mongoose.connect(MONGOURI, {
 	useUnifiedTopology: true,
